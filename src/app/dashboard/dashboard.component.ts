805560dc4878f6f61e678070ae07045905111d3a
Sharmila Thirumalainathan <sh946344@dal.ca>
2019-05-31 03:21:11 -0300

    Dashboard and responsive Changes

--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -5,11 +5,42 @@ import { Component, OnInit } from '@angular/core';
   templateUrl: './dashboard.component.html',
   styleUrls: ['./dashboard.component.scss']
 })
+
+
 export class DashboardComponent implements OnInit {
+
+  courses = [{
+    id: 1,
+    title: "Data Management",
+    code: "CSCI 1234",
+    join_code: 6789
+  }, {
+    id: 2,
+    title: "Web Development",
+    code: "CSCI 5709",
+    join_code: 7282
+  }, {
+    id: 3,
+    title: "Cloud Computing",
+    code: "CSCI 5701",
+    join_code: 9281
+  }, {
+    id: 4,
+    title: "Web Development",
+    code: "CSCI 5709",
+    join_code: 7282
+  }];
 
   constructor() { }
 
   ngOnInit() {
   }
 
+  test() {
+    if (document.getElementById("wrapper").classList.contains("collapse")) {
+      document.getElementById("wrapper").classList.remove("collapse");
+    } else {
+      document.getElementById("wrapper").classList.add("collapse");
+    }
+  }
 }
