1780e3f8741b18e918decfa144492d8e5dbf2285
Sharmila Thirumalainathan <sh946344@dal.ca>
2019-07-30 20:14:51 -0300

    Conflict fix

--- Backend/app/api/controllers/CreatePostController.js
+++ Backend/app/api/controllers/CreatePostController.js
@@ -7,37 +7,31 @@
 
 module.exports = {
 
-create: function(request, response) {
-  var post = request.allParams();
-  post.messageCount = 0;
-  Post.create(post, function(err, user) {
-    if (!err) {
-      return response.status(201).send({
-        message: 'Post Added Successfully!!'
+  create: function(request, response) {
+    var post = request.allParams();
+    post.messageCount = 0;
+    Post.create(post, function(err, user) {
+      if (!err) {
+        return response.status(201).send({
+          message: 'Post Added Successfully!!'
+        });
+      }
+      console.log(err);
+      return response.status(500).send({
+        message: 'Can Not Add Post!!'
       });
-    }
-    console.log(err);
-    return response.status(500).send({
-      message: 'Can Not Add Post!!'
     });
-  });
-}
-console.log(err);
-return response.status(500).send({
-  message: 'Can Not Add Post!!'
-});
-});
-},
+  },
 
-getCourses: function(request, response) {
-  var user = request.allParams();
-  Enroll.find({
-    email: user.email
-  }).exec(function(err, data) {
-    if (err) return next(err);
-    response.json(data);
-    console.log(data);
-  });
-}
+  getCourses: function(request, response) {
+    var user = request.allParams();
+    Enroll.find({
+      email: user.email
+    }).exec(function(err, data) {
+      if (err) return next(err);
+      response.json(data);
+      console.log(data);
+    });
+  }
 
 }
