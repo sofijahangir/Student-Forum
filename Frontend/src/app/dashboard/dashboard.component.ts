fd6a51f55b4bc0f37518311a12e4e5ae5c8b88c2
niravs200 <niravs200@gmail.com>
2019-07-31 01:34:28 -0300

    Adding image to dashboard

--- Frontend/src/app/dashboard/dashboard.component.ts
+++ Frontend/src/app/dashboard/dashboard.component.ts
@@ -4,6 +4,7 @@ import { StoreService } from '../store.service';
 import Swal from 'sweetalert2';
 import { SwPush } from '@angular/service-worker';
 import { WebWorkerService } from 'ngx-web-worker';
+import { DomSanitizer } from '@angular/platform-browser';
 
 @Component({
   selector: 'app-dashboard',
@@ -38,6 +39,7 @@ export class DashboardComponent implements OnInit {
       .catch(err => console.error(err));
   }
 
+
   sendToServer(params: any) {
     var email = sessionStorage.getItem("email");
     this.store.post('/notifications', { notification: params, email: email }).subscribe(sub => {
@@ -54,6 +56,7 @@ export class DashboardComponent implements OnInit {
         sessionStorage.setItem("userName", res[0].name);
       }
 
+
       this.store.post('/getDashboardCourses').subscribe((res) => {
         var length = Object.keys(res).length;
         for (var i = 0; i < length; i++) {
@@ -61,10 +64,12 @@ export class DashboardComponent implements OnInit {
           course["title"] = res[i].coursename;
           course["code"] = res[i].coursecode;
           if (res[i].image == "") {
-            course["image"] = "data:image/svg+xml;charset=UTF-8,%3Csvg%20width%3D%22200%22%20height%3D%22250%22%20xmlns%3D%22http%3A%2F%2Fwww.w3.org%2F2000%2Fsvg%22%20viewBox%3D%220%200%20200%20250%22%20preserveAspectRatio%3D%22none%22%3E%3Cdefs%3E%3Cstyle%20type%3D%22text%2Fcss%22%3E%23holder_16b06d0c713%20text%20%7B%20fill%3A%23eceeef%3Bfont-weight%3Abold%3Bfont-family%3AArial%2C%20Helvetica%2C%20Open%20Sans%2C%20sans-serif%2C%20monospace%3Bfont-size%3A13pt%20%7D%20%3C%2Fstyle%3E%3C%2Fdefs%3E%3Cg%20id%3D%22holder_16b06d0c713%22%3E%3Crect%20width%3D%22200%22%20height%3D%22250%22%20fill%3D%22%2360636b%22%3E%3C%2Frect%3E%3Cg%3E%3Ctext%20x%3D%2256.1953125%22%20y%3D%22131%22%3EThumbnail%3C%2Ftext%3E%3C%2Fg%3E%3C%2Fg%3E%3C%2Fsvg%3E";
+            var im = "data:image/svg+xml;charset=UTF-8,%3Csvg%20width%3D%22200%22%20height%3D%22250%22%20xmlns%3D%22http%3A%2F%2Fwww.w3.org%2F2000%2Fsvg%22%20viewBox%3D%220%200%20200%20250%22%20preserveAspectRatio%3D%22none%22%3E%3Cdefs%3E%3Cstyle%20type%3D%22text%2Fcss%22%3E%23holder_16b06d0c713%20text%20%7B%20fill%3A%23eceeef%3Bfont-weight%3Abold%3Bfont-family%3AArial%2C%20Helvetica%2C%20Open%20Sans%2C%20sans-serif%2C%20monospace%3Bfont-size%3A13pt%20%7D%20%3C%2Fstyle%3E%3C%2Fdefs%3E%3Cg%20id%3D%22holder_16b06d0c713%22%3E%3Crect%20width%3D%22200%22%20height%3D%22250%22%20fill%3D%22%2360636b%22%3E%3C%2Frect%3E%3Cg%3E%3Ctext%20x%3D%2256.1953125%22%20y%3D%22131%22%3EThumbnail%3C%2Ftext%3E%3C%2Fg%3E%3C%2Fg%3E%3C%2Fsvg%3E";
+            course["image"] = this._DomSanitizationService.bypassSecurityTrustUrl(im);
           }
           else {
-            course["image"] = res[i].image;
+            im = res[i].image;
+            course["image"] = this._DomSanitizationService.bypassSecurityTrustUrl(im);
           }
           this.courses.push(course);
         }
