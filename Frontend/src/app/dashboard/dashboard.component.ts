a3aa0f10bdcdd8afc81268547819ee135e8bf91b
niravs200 <niravs200@gmail.com>
2019-07-31 01:09:53 -0300

    Dashboard Partially complete

--- Frontend/src/app/dashboard/dashboard.component.ts
+++ Frontend/src/app/dashboard/dashboard.component.ts
@@ -54,63 +54,47 @@ export class DashboardComponent implements OnInit {
         sessionStorage.setItem("userName", res[0].name);
       }
 
-    }, err => {
-      console.log(err);
-    });
+      this.store.post('/getDashboardCourses').subscribe((res) => {
+        var length = Object.keys(res).length;
+        for (var i = 0; i < length; i++) {
+          var course = {};
+          course["title"] = res[i].coursename;
+          course["code"] = res[i].coursecode;
+          if (res[i].image == "") {
+            course["image"] = "data:image/svg+xml;charset=UTF-8,%3Csvg%20width%3D%22200%22%20height%3D%22250%22%20xmlns%3D%22http%3A%2F%2Fwww.w3.org%2F2000%2Fsvg%22%20viewBox%3D%220%200%20200%20250%22%20preserveAspectRatio%3D%22none%22%3E%3Cdefs%3E%3Cstyle%20type%3D%22text%2Fcss%22%3E%23holder_16b06d0c713%20text%20%7B%20fill%3A%23eceeef%3Bfont-weight%3Abold%3Bfont-family%3AArial%2C%20Helvetica%2C%20Open%20Sans%2C%20sans-serif%2C%20monospace%3Bfont-size%3A13pt%20%7D%20%3C%2Fstyle%3E%3C%2Fdefs%3E%3Cg%20id%3D%22holder_16b06d0c713%22%3E%3Crect%20width%3D%22200%22%20height%3D%22250%22%20fill%3D%22%2360636b%22%3E%3C%2Frect%3E%3Cg%3E%3Ctext%20x%3D%2256.1953125%22%20y%3D%22131%22%3EThumbnail%3C%2Ftext%3E%3C%2Fg%3E%3C%2Fg%3E%3C%2Fsvg%3E";
+          }
+          else {
+            course["image"] = res[i].image;
+          }
+          this.courses.push(course);
+        }
+      }, err => {
+        console.log(err);
+      });
 
-  }
+    }
 
-  courses = [{
-    id: 1,
-    title: "Data Management",
-    code: "CSCI 1234",
-    join_code: 6789
-  }, {
-    id: 2,
-    title: "Web Development",
-    code: "CSCI 5709",
-    join_code: 7282
-  }, {
-    id: 3,
-    title: "Cloud Computing",
-    code: "CSCI 5701",
-    join_code: 9281
-  }, {
-    id: 4,
-    title: "Software Development Concepts",
-    code: "CSCI 5709",
-    join_code: 4536
-  }, {
-    id: 5,
-    title: "Mobile Computing",
-    code: "CSCI 5609",
-    join_code: 8362
-  }, {
-    id: 6,
-    title: "Visual Analytics",
-    code: "CSCI 5929",
-    join_code: 9172
-  }];
+  courses = [];
 
-  searchCourse(value: string) {
-    if (value) {
-      this.router.navigate(['/course/browse'], { queryParams: { keyword: value } });
+    searchCourse(value: string) {
+      if (value) {
+        this.router.navigate(['/course/browse'], { queryParams: { keyword: value } });
+      }
+    }
+
+    collapse() {
+      if (document.getElementById("wrapper").classList.contains("collapse")) {
+        document.getElementById("wrapper").classList.remove("collapse");
+      } else {
+        document.getElementById("wrapper").classList.add("collapse");
+      }
+    }
+
+    logout() {
+      this.store.post('/signout').subscribe((res) => {
+        this.router.navigate(['/home']);
+      }, err => {
+        Swal.fire('Oops..', 'Something Went Wrong', 'error')
+      });
     }
   }
-
-  collapse() {
-    if (document.getElementById("wrapper").classList.contains("collapse")) {
-      document.getElementById("wrapper").classList.remove("collapse");
-    } else {
-      document.getElementById("wrapper").classList.add("collapse");
-    }
-  }
-
-  logout() {
-    this.store.post('/signout').subscribe((res) => {
-      this.router.navigate(['/home']);
-    }, err => {
-      Swal.fire('Oops..', 'Something Went Wrong', 'error')
-    });
-  }
-}
